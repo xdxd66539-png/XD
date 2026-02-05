145f7fc7444d9d3686487f6ffaeb69e2885e6cc5
xdxd66539-png <xdxd66539@gmail.com>
2026-02-05 17:30:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
   res.send("Bot aktif ve Render üzerinde çalışıyor!");
